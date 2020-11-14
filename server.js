def9c14593f1c875cc29c62e2c54dfa40083753a
WahidX <wahidx93@gmail.com>
2020-11-15 01:44:07 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const port = process.env.PORT || 8000;
+// const expressEjsLayouts = require('express-ejs-layouts');
+
+const app = express();
+// require('./config/view_helpers')(app);
+
+app.get('/', function(req, res){
+
+	let data = {
+		'name': 'Droid',
+		'hobby': 'Gaming'
+	};
+
+	return res.json(data);
+});
+
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log(`Error while running server: ${err}`);
+    }
+
+    console.log(`Server running at ${port}`);
+})
